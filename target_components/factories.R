768c2c84b5fd78f50c42ef88a5d045f9e233b061
karltayeb <karl.tayeb7@gmail.com>
2022-01-12 21:13:35 -0600

    add method name when scoring...

--- target_components/factories.R
+++ target_components/factories.R
@@ -82,7 +82,7 @@ generate_score_target <- function(prefix, method.name, prediction_col){
       expr(!!target_fit %>% rowwise() %>% mutate(
         scores = list(get.score(fit[[!!prediction_col]][[1]], sim$active)),
         score.cs = if(grepl('susie', !!method_name)) {list(score.credible.set(fit, sim$active))} else {list(NULL)},
-        method=method_name
+        method = !!method_name
       )), pattern=expr(!!target_pattern)
     )
   )
