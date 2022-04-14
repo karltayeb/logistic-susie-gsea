47e558c60e9b8a13d74611621812ff5e44378d5e
karltayeb <karl.tayeb7@gmail.com>
2022-04-14 18:20:50 -0500

    moderate optimizations

--- code/logistic_susie_vb.R
+++ code/logistic_susie_vb.R
@@ -4,11 +4,35 @@
 ### If this idea is worth pursuing further, then this code can be improved
 
 library(BiocGenerics)  # TODO: don't pollute global namespace with this
+library(matrixStats)
 
-fast_matmul_X = function(X, A){
-
+colScale = function(x, scale = TRUE) {
+    x = t((t(x) * scale))
+    return(x)
 }
-calc_Q = function(X, Sigma2, Mu, Alpha, Z, delta) {
+
+centeredmatmul = function(dat, A){
+  n <- dim(dat$X.raw)[1]
+  res <- dat$X.raw %*% A
+  if(dat$center){
+    if(is.null(dim(A))){
+      res <- res - sum(dat$X.shift * A)
+    } else{
+      res <- t(t(res) - (dat$X.shift %*% A)[1,])
+    }
+  }
+  return(res)
+}
+
+test_cmm = function(dat){
+  p <- dim(dat$X)[2]
+  A <- matrix(rnorm(10*p), nrow = p)
+  a <- dat$X %*% A
+  b <- centeredmatmul(dat, A)
+  all.equal(a, b)
+}
+
+calc_Q = function(dat, Sigma2, Mu, Alpha, Z, delta) {
   # X is data matrix
   # Sigma2[j, l] is the posterior variance for b_l when entry j is selected, p x L
   # Mu[j, l] is the posterior mean for b_l when entry j is selected, p x L
@@ -16,15 +40,15 @@ calc_Q = function(X, Sigma2, Mu, Alpha, Z, delta) {
   # Z is matrix of covariates (e.g. column for intercept, top 10 PCs, etc)
   # delta is current estimate for effects of Z variables
   ASU2 = Alpha * (Sigma2 + Mu^2) # [j, l] = alpha[j, l] * (Sigma2[j, l] + Mu[j, l]^2)
-  Q = BiocGenerics::rowSums(X^2 %*% ASU2) # start w/ sum_l E_(ql)[(x_i' b_l)^2]
+  Q = BiocGenerics::rowSums(dat$X2 %*% ASU2) # start w/ sum_l E_(ql)[(x_i' b_l)^2]
 
   # now add 2 sum_l sum_{k>l} (x_i' b_l_post)(x_i' b_k_post)
   b_post_mat = Mu * Alpha # each column is posterior mean for b_l, p x L
-  X_b_post = X %*% b_post_mat # [i, l] = x_i' b_l_post
+  X_b_post = dat$X %*% b_post_mat # [i, l] = x_i' b_l_post
   Q = Q + BiocGenerics::rowSums(X_b_post)^2 - BiocGenerics::rowSums(X_b_post^2)
 
   # now, add other terms with Z and delta
-  Q = Q + as.numeric(2 * (X %*% rowSums(b_post_mat)) * (Z %*% delta)) + as.numeric((Z %*% delta)^2)
+  Q = Q + as.numeric(2 * (dat$X %*% rowSums(b_post_mat)) * (Z %*% delta)) + as.numeric((Z %*% delta)^2)
   return(Q)
 }
 
@@ -33,7 +57,7 @@ g = function(x) { # ilogit function
 }
 
 
-update_xi = function(X, Sigma2, Mu, Alpha, Z, delta) {
+update_xi = function(dat, Sigma2, Mu, Alpha, Z, delta) {
   # X is data matrix
   # Sigma2[j, l] is the posterior variance for b_l when entry j is selected, p x L
   # Mu[j, l] is the posterior mean for b_l when entry j is selected, p x L
@@ -41,7 +65,7 @@ update_xi = function(X, Sigma2, Mu, Alpha, Z, delta) {
   # Z is matrix of covariates (e.g. column for intercept, top 10 PCs, etc)
   # delta is current estimate for effects of Z variables
 
-  Q = calc_Q(X, Sigma2, Mu, Alpha, Z, delta)
+  Q = calc_Q(dat, Sigma2, Mu, Alpha, Z, delta)
 
   xi = sqrt(Q)
 
@@ -50,7 +74,7 @@ update_xi = function(X, Sigma2, Mu, Alpha, Z, delta) {
 }
 
 
-update_b_l = function(X, y, xi, prior_weights, V, Sigma2, Mu, Alpha, l, Z, delta) {
+update_b_l = function(dat, xi, prior_weights, V, Sigma2, Mu, Alpha, l, Z, delta) {
   # X is data matrix
   # y is binary response
   # xi is lower-bound approximation parameters
@@ -69,10 +93,12 @@ update_b_l = function(X, y, xi, prior_weights, V, Sigma2, Mu, Alpha, l, Z, delta
   g_xi_5_xi = ((g_xi - .5) / xi) # vector of (g(xi_i) - .5) / xi_i, pre-compute once
   g_xi_5_xi[xi == 0] = .25 # case of 0/0 (e.g. x_i is all 0), use L'Hopital
 
-  common_denoms = (1 / V) + (t(X^2) %*% g_xi_5_xi) # appears many times, compute once, posterior precisions
+  # common_denoms = (1 / V) + (t(X^2) %*% g_xi_5_xi) # appears many times, compute once, posterior precisions
+  common_denoms = (1 / V) + g_xi_5_xi %*% dat$X2 # appears many times, compute once, posterior precisions
 
   # update Alpha[, l]
-  nums = t(X) %*% (y - .5 -  (g_xi_5_xi * ((X %*% b_post_not_l) + (Z %*% delta)))) # numerator in exp
+  tmp <- centeredmatmul(dat, b_post_not_l)[,1]
+  nums = (dat$y - .5 -  (g_xi_5_xi * (tmp + (Z %*% delta)[,1]))) %*% dat$X # numerator in exp
   Alpha[, l] = as.numeric(log(prior_weights) + (nums^2 / (2*common_denoms)) - (1/2)*log(common_denoms))
   Alpha[, l] = exp(Alpha[, l] - max(Alpha[, l])) # remove max for stability, everything still proportional
   Alpha[, l] = Alpha[, l] / sum(Alpha[, l]) # normalize, sum to 1
@@ -82,13 +108,11 @@ update_b_l = function(X, y, xi, prior_weights, V, Sigma2, Mu, Alpha, l, Z, delta
 
   # update Sigma[, l]
   Sigma2[, l] = as.numeric(1 / common_denoms)
-
   return(list(Sigma2 = Sigma2, Mu = Mu, Alpha = Alpha))
-
 }
 
 
-update_delta = function(X, y, xi, Mu, Alpha, Z) {
+update_delta = function(dat, xi, Mu, Alpha, Z) {
   # X is data matrix
   # y is binary response
   # xi is lower-bound approximation parameters
@@ -102,9 +126,9 @@ update_delta = function(X, y, xi, Mu, Alpha, Z) {
 
   #D = Matrix::Diagonal(x = g_xi_5_xi / 2)
   D = Matrix::Diagonal(x = g_xi_5_xi)
-  ZtDZ = t(Z) %*% D %*% Z
-  DXB = D %*% X %*% rowSums(Mu * Alpha)
-  RHS = t(Z) %*% (y - .5 - DXB) # RHS of system LL'delta = RHS
+  ZtDZ = (t(Z) * g_xi_5_xi) %*% Z  # D %*% Z
+  DXB = colScale(dat$X %*% rowSums(Mu * Alpha), g_xi_5_xi)#  %*% rowSums(Mu * Alpha)
+  RHS = t(Z) %*% (dat$y - .5 - DXB) # RHS of system LL'delta = RHS
 
   # NOTE: the below system can/should be solved w/ Cholesky and forward/backward substitution (but if Z has small # of columns, shouldn't matter)
   #Lt = chol(ZtDZ)
@@ -118,9 +142,7 @@ update_delta = function(X, y, xi, Mu, Alpha, Z) {
 
 }
 
-update_all = function(X,
-  X,
-                      y,
+update_all = function(dat,
                       xi,
                       prior_weights,
                       V,
@@ -132,7 +154,7 @@ update_all = function(X,
                       estimate_prior_variance,
                       share_prior_variance,
                       intercept) {
-  # X is data matrix
+  #  X is data matrix
   # y is binary response
   # xi is lower-bound approximation parameters
   # prior_weights is prior probabilities for selecting j (p-vector)
@@ -148,7 +170,7 @@ update_all = function(X,
 
   # update delta
   if (intercept) { # if covariates and/or intercept
-    delta = update_delta(X, y, xi, Mu, Alpha, Z)
+    delta = update_delta(dat, xi, Mu, Alpha, Z)
   }
 
   if (length(V) == L) {
@@ -162,14 +184,14 @@ update_all = function(X,
 
   # now, iterate over l = 1:L
   for (l in 1:L) {
-    res_l = update_b_l(X, y, xi, prior_weights, prior_vars[l], Sigma2, Mu, Alpha, l, Z, delta)
+    res_l = update_b_l(dat, xi, prior_weights, prior_vars[l], Sigma2, Mu, Alpha, l, Z, delta)
     Sigma2 = res_l$Sigma2
     Mu = res_l$Mu
     Alpha = res_l$Alpha
   }
 
   # now, update xi
-  xi = update_xi(X, Sigma2, Mu, Alpha, Z, delta)
+  xi = update_xi(dat, Sigma2, Mu, Alpha, Z, delta)
 
   if (estimate_prior_variance == TRUE) { # if estimating prior variance
     ASU2 = Alpha * (Sigma2 + Mu^2) # [j, l] = alpha[j, l] * (Sigma2[j, l] + Mu[j, l]^2)
@@ -239,8 +261,9 @@ logistic.susie.init = function(
   p = ncol(X)
   n = nrow(X)
 
-  X.shift <- colMeans(X)
-  X.scale <- sqrt(sparseMatrixStats::colVars(X))
+  se <- sqrt(sparseMatrixStats::colVars(X))
+  X.raw <- X
+  X.shift <- colMeans(X.raw)
 
   if(standardize){
     X <- scale(X)
@@ -261,6 +284,16 @@ logistic.susie.init = function(
     prior_weights = rep(1 / p, p)
   }
 
+  #' save all the things that don't change here
+  dat <- list(
+    X=X, X2=X^2,
+    X.raw = X.raw, X2.raw = X.raw^2, X.shift=X.shift, X.scale=se,
+    y=y, Z=Z, prior_weights=prior_weights,
+    intercept=intercept,
+    estimate_prior_variance=estimate_prior_variance,
+    share_prior_variance=share_prior_variance,
+    standardize = standardize, center = center)
+
   # place to store posterior info for each l = 1, ..., L
   # initialize: could think of something better
   #delta = glm(y ~ Z - 1, family = "binomial")$coef # initialize to regular GLM solution
@@ -273,41 +306,33 @@ logistic.susie.init = function(
   Alpha = matrix(prior_weights, nrow = p, ncol = L)
   Mu = matrix(0, nrow = p, ncol = L)
   Sigma2 = matrix(V, nrow = p, ncol = L, byrow = T)
-  xi = update_xi(X, Sigma2, Mu, Alpha, Z, delta)
+  xi = update_xi(dat, Sigma2, Mu, Alpha, Z, delta)
 
   post_info = list(Sigma2 = Sigma2, Mu = Mu, Alpha = Alpha, delta = delta, xi = xi, V = V)
-    X=X, X.scale=X.scale, X.shift=shift,
-    X=X, X.scale=X.scale, X.shift=X.shift,
-    y=y, Z=Z, prior_weights=prior_weights,
-    intercept=intercept,
-    estimate_prior_variance=estimate_prior_variance,
-    share_prior_variance=share_prior_variance,
-    standardize=standardize,
-    scale=scale
-  )
+
   elbo <- calc_ELBO(
-    dat$y, dat$X, post_info$Alpha, post_info$Mu, post_info$Sigma2, post_info$V,
+    dat, post_info$Alpha, post_info$Mu, post_info$Sigma2, post_info$V,
     dat$prior_weights, dat$Z, post_info$delta, post_info$xi
   )
   res = list(dat=dat, post_info=post_info, elbo=elbo)
   return(res)
 }
 
+logistic.susie.iteration = function(res, compute_elbo=T){
   dat <- res$dat
   post_info <- res$post_info
   post_info  <- update_all(
-    dat$X, dat$y, post_info$xi, dat$prior_weights,
+    dat, post_info$xi, dat$prior_weights,
     post_info$V, post_info$Sigma2, post_info$Mu,
     post_info$Alpha, dat$Z, post_info$delta,
     dat$estimate_prior_variance, dat$share_prior_variance, dat$intercept)
   res$post_info <- post_info
-  res$post_info <- update_all(res)
+  if(compute_elbo){
     elbo <- calc_ELBO(
-      dat$y, dat$X, post_info$Alpha, post_info$Mu, post_info$Sigma2, post_info$V,
+      dat, post_info$Alpha, post_info$Mu, post_info$Sigma2, post_info$V,
       dat$prior_weights, dat$Z, post_info$delta, post_info$xi
     )
     res$elbo <- c(res$elbo, elbo)
-    res$elbo <- c(res$elbo, new.elbo)
   }
   return(res)
 }
@@ -338,7 +363,7 @@ logistic.susie.wrapup = function(res){
 # CAREFUL: Not sure what to do when Alpha[j, l] = 0 (we get 0*ln(0)). I will set this to 0
 # Note: This expression is only valid when xi has been updated to be sqrt(Q), where Q is the
 # expectation of the square of the linear predictor under the variational posterior (what we update xi to nomrally)
-calc_ELBO = function(y, X, Alpha, Mu, Sigma2, V, prior_weights, Z, delta, xi) {
+calc_ELBO = function(dat, Alpha, Mu, Sigma2, V, prior_weights, Z, delta, xi) {
   p = nrow(Mu)
   L = ncol(Mu)
   P = matrix(prior_weights, nrow = p, ncol = L)
@@ -346,7 +371,7 @@ calc_ELBO = function(y, X, Alpha, Mu, Sigma2, V, prior_weights, Z, delta, xi) {
 
   V = matrix(V, nrow = p, ncol = L, byrow = T) # for proper arithmetic, either if V is a scalar or a vector of length L
 
-  expected_log_lik = sum(log(g(xi)) + (y - .5) * as.numeric(((X %*% b_post) + (Z %*% delta))) - (xi / 2))
+  expected_log_lik = sum(log(g(xi)) + (dat$y - .5) * as.numeric(((dat$X %*% b_post) + (Z %*% delta))) - (xi / 2))
   KL_div_vb_prior = Alpha * (log(Alpha) - log(P) + (log(V) / 2) - (log(Sigma2) / 2) - .5 + ((Sigma2 + Mu^2) / (2 * V)))
   KL_div_vb_prior[Alpha == 0] = 0
   KL_div_vb_prior = sum(KL_div_vb_prior)
