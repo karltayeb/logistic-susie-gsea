6717cfce40f720be20e1f460d424f4776f516bce
karltayeb <karl.tayeb7@gmail.com>
2022-05-03 09:30:49 -0500

    todel

--- code/enrichment_pipeline.R
+++ code/enrichment_pipeline.R
@@ -7,13 +7,13 @@
 prep_binary_data = function(gs, dat, thresh, .sign=c(1, -1)) {
   # get common genes as background
   gs.genes <- rownames(gs$X)
-  
+
   # filter gene list
   y <- dat %>%
     filter(ENTREZID %in% gs.genes, !is.na(threshold.on)) %>%
-    mutate(threshold.on = threshold.on * as.integer(sign(beta) == .sign))
+    mutate(threshold.on = if_else(sign(beta) %in% .sign, threshold.on, 1)) %>%
     mutate(geneList = as.integer(threshold.on < thresh)) %>%
-    select(ENTREZID, geneList) %>% 
+    select(ENTREZID, geneList) %>%
     distinct(across(ENTREZID), .keep_all = T) %>%
     tibble2namedlist()
 
@@ -24,10 +24,41 @@ prep_binary_data = function(gs, dat, thresh, .sign=c(1, -1)) {
   bad.cols <- BiocGenerics::colSums(X)
   bad.cols <- (bad.cols == 0) | bad.cols == length(test.genes)
   X <- X[, which(!bad.cols)]
-  
+
   # reorder genes in y to match X order
   y <- y[rownames(X)]
   return(list(y=y, X=X))
+}
+
+prep_sumstat_data = function(gs, dat) {
+  # get common genes as background
+  gs.genes <- rownames(gs$X)
+
+  # filter gene list
+  subset_dat <- dat %>%
+    filter(ENTREZID %in% gs.genes, !is.na(beta), !is.na(se)) %>%
+    distinct(across(ENTREZID), .keep_all = T)
+
+  beta <- subset_dat %>%
+    select(ENTREZID, beta) %>%
+    tibble2namedlist()
+
+  se <- subset_dat %>%
+    select(ENTREZID, se) %>%
+    tibble2namedlist()
+
+  # filter gene sets
+  y.genes <- names(beta)
+  test.genes <- intersect(gs.genes, y.genes)
+  X <- gs$X[test.genes,]
+  bad.cols <- BiocGenerics::colSums(X)
+  bad.cols <- (bad.cols == 0) | bad.cols == length(test.genes)
+  X <- X[, which(!bad.cols)]
+
+  # reorder genes in y to match X order
+  beta <- beta[rownames(X)]
+  se <- se[rownames(X)]
+  return(list(beta=beta, se=se, X=X))
 }
 
 do_logistic_susie = function(experiment,
@@ -37,16 +68,20 @@ do_logistic_susie = function(experiment,
                              data,
                              susie.args=NULL,
                              .sign=c(1, -1)) {
-  cat(paste0('Fitting logistic susie...\n\tExperiment = ', experiment, '\n\tDatabase = ', db, '\n\tthresh = ', thresh))
+  cat(paste0('Fitting logistic susie...',
+    '\n\tExperiment = ', experiment,
+    '\n\tDatabase = ', db,
+    '\n\tthresh = ', thresh))
+
   gs <- genesets[[db]]
   dat <- data[[experiment]]
   u <- prep_binary_data(gs, dat, thresh, .sign)  # subset to common genes
-  
+
   if(is.null(susie.args)){  # default SuSiE args
     susie.args = list(
       L=10, init.intercept=0, verbose=1, maxit=500, standardize=FALSE)
   }
-  vb.fit <- exec(logistic.susie, u$X, u$y, !!!susie.args)
+  vb.fit <- exec(gseareg::fit_logistic_susie, u$X, u$y, !!!susie.args)
   res = tibble(
     experiment=experiment,
     db=db,
@@ -67,7 +102,7 @@ do_logistic_susie_veb = function(experiment,
   gs <- genesets[[db]]
   dat <- data[[experiment]]
   u <- prep_binary_data(gs, dat, thresh, .sign)  # subset to common genes
-  
+
   if(is.null(susie.args)){  # default SuSiE args
     susie.args = list(L=10)
   }
@@ -81,17 +116,43 @@ do_logistic_susie_veb = function(experiment,
   return(res)
 }
 
+do_tccm_point_normal_susie = function(experiment,
+                             db,
+                             genesets,
+                             data,
+                             susie.args=NULL) {
+  cat(paste0(
+    'Fitting point-normal susie...',
+    '\n\tExperiment = ', experiment,
+    '\n\tDatabase = ', db))
+  gs <- genesets[[db]]
+  dat <- data[[experiment]]
+  u <- prep_sumstat_data(gs, dat)  # subset to common genes
+
+  if(is.null(susie.args)){  # default SuSiE args
+    susie.args = list(L=10, verbose=T, maxit=500)
+  }
+  fit <- exec(tccm_point_normal_susie, u$beta, u$se, u$X, !!!susie.args)
+  res = tibble(
+    experiment=experiment,
+    db=db,
+    fit=list(fit),
+    susie.args = list(susie.args)
+  )
+  return(res)
+}
+
 do_ora = function(experiment,
                   db,
                   thresh,
                   genesets,
                   data,
-                  .sign=c(1,-1){
-  
+                  .sign=c(1,-1)){
+
   gs <- genesets[[db]]
   dat <- data[[experiment]]
   u <- prep_binary_data(gs, dat, thresh, .sign)  # subset to common genes
-  
+
   do_fet = function(overlap, geneListSize, geneSetSize, nGenes){
     ct <- matrix(c(
       overlap,
@@ -100,7 +161,7 @@ do_ora = function(experiment,
       nGenes - geneSetSize - geneListSize + overlap), nr=2)
     return(fisher.test(ct)$p.value)
   }
-  
+
   do_hyper = function(overlap, geneListSize, geneSetSize, nGenes){
     # genes in list = white balls, not in list = black balls
     # draw k balls w/o replacement where k is the size of the gene set
@@ -112,7 +173,7 @@ do_ora = function(experiment,
       geneSetSize,
       lower.tail = FALSE))
   }
-  
+
   ora <- tibble(
       geneSet = colnames(u$X),
       geneListSize = sum(u$y),
@@ -121,16 +182,16 @@ do_ora = function(experiment,
       nGenes = length(u$y),
       propInList = overlap / geneListSize,
       propInSet = overlap / geneSetSize,
-      oddsRatio = (overlap / (geneListSize - overlap)) / (
-        (geneSetSize - overlap) / (nGenes - geneSetSize + overlap))
+      oddsRatio = (overlap * (nGenes - geneSetSize - geneListSize + overlap)) /
+        ((geneListSize-overlap) * (geneSetSize - overlap))
     ) %>%
     rowwise() %>%
     mutate(
       pHypergeometric = do_hyper(overlap, geneListSize, geneSetSize, nGenes),
       pFishersExact = do_fet(overlap, geneListSize, geneSetSize, nGenes)
-    ) %>% 
+    ) %>%
     ungroup()
-  
+
   res = tibble(
     experiment=experiment,
     db=db,
@@ -147,14 +208,14 @@ get_credible_set_summary = function(res){
     rownames_to_column(var='geneSet') %>%
     rename_with(~str_replace(., 'X', 'L')) %>%
     dplyr::rename(L1 = 2) %>%  # rename deals with L=1 case
-    pivot_longer(starts_with('L'), names_to='component', values_to = 'beta') 
+    pivot_longer(starts_with('L'), names_to='component', values_to = 'beta')
   se <- t(sqrt(res$mu2 - res$mu^2)) %>%
      data.frame() %>%
       rownames_to_column(var='geneSet') %>%
       rename_with(~str_replace(., 'X', 'L')) %>%
       dplyr::rename(L1 = 2) %>%  # rename deals with L=1 case
       pivot_longer(starts_with('L'), names_to='component', values_to = 'beta.se')
-  
+
   credible.set.summary <- t(res$alpha) %>%
     data.frame() %>%
     rownames_to_column(var='geneSet') %>%
@@ -175,7 +236,7 @@ get_credible_set_summary = function(res){
 get_gene_set_summary = function(res){
   #' map each gene set to the component with top alpha
   #' report pip
-  res$pip %>% 
+  res$pip %>%
     as_tibble(rownames='geneSet') %>%
     dplyr::rename(pip=value) %>%
     mutate(beta=colSums(res$alpha * res$mu))
@@ -192,6 +253,80 @@ pack_group = function(tbl){
     }
     return(res)
 }
+
+
+label_sig_enrichments = function(tbl){
+   tbl %>% mutate(
+    padj = p.adjust(pFishersExact),
+    result = case_when(
+      padj < 0.05 & oddsRatio < 1 ~ 'depleted',
+      padj < 0.05 & oddsRatio > 1 ~ 'enriched',
+      TRUE ~ 'not significant'
+    )
+  )
+}
+
+do.volcano = function(res){
+  res %>%
+    label_sig_enrichments %>%
+    ggplot(aes(x=log10(oddsRatio), y=-log10(pFishersExact), color=result)) +
+    geom_point() +
+    geom_point(
+      res %>% filter(in_cs, active_cs),
+      mapping=aes(x=log10(oddsRatio), y=-log10(pFishersExact)),
+      color='black', pch=21, size=5) +
+    scale_color_manual(values = c('depleted' = 'coral',
+                                  'enriched' = 'dodgerblue',
+                                  'not significant' = 'grey'))
+}
+
+
+# take credible set summary, return "best" row for each gene set
+get_cs_summary_condensed = function(fit){
+  fit %>%
+    get_credible_set_summary() %>%
+    group_by(geneSet) %>%
+    arrange(desc(alpha)) %>%
+    filter(row_number() == 1)
+}
+# generate table for making gene-set plots
+get_plot_tbl = function(fits, ora){
+  res <- fits %>%
+    left_join(ora) %>%
+    mutate(
+      gs_summary = map(fit, get_gene_set_summary),
+      cs_summary = map(fit, get_cs_summary_condensed),
+      res = map2(gs_summary, cs_summary, ~ left_join(.x, .y, by='geneSet')),
+      res = map2(res, ora, ~ possibly(left_join, NULL)(.x, .y))
+    ) %>%
+    select(-c(fit, susie.args, ora, gs_summary, cs_summary)) %>%
+    unnest(res)
+  return(res)
+}
+
+# split tibble into a list using 'col'
+split_tibble = function(tibble, col = 'col'){
+  tibble %>% split(., .[, col])
+}
+
+# Get summary of credible sets with gene set descriptions
+get_table_tbl = function(fits, ora){
+  res2 <- fits %>%
+    left_join(ora) %>%
+    mutate(res = map(fit, get_credible_set_summary)) %>%
+    mutate(res = map2(res, ora, ~ left_join(.x, .y))) %>%
+    select(-c(fit, ora)) %>%
+    unnest(res)
+
+  descriptions <- map_dfr(genesets, ~pluck(.x, 'geneSet', 'geneSetDes'))
+  tbl <- res2 %>%
+    filter(active_cs) %>%
+    left_join(descriptions)
+  tbl_split <- split_tibble(tbl, 'experiment')
+  html_tables <- map(tbl_split, ~split_tibble(.x, 'db'))
+  return(html_tables)
+}
+
 
 #' Report credible set based summary of SuSiE
 #' @param tbl output of \alias{get_credible_set_summary} to be formatted
@@ -218,15 +353,4 @@ report_susie_credible_sets = function(tbl,
     pack_group %>%
     column_spec(c(4), color=ifelse(tbl_filtered$beta >0, 'green', 'red')) %>%
     kableExtra::kable_styling()
-}
-
-
-make_susie_html_table = function(fits, db, experiment, thresh){
-  require(htmltools)
-  fits %>%
-    pluck(db) %>%
-    pluck(topic) %>%
-    get_credible_set_summary() %>%
-    filter(active_cs, in_cs) %>%
-    report_susie_credible_sets(target_coverage = 0.95, max_coverage = 0.95)
 }
