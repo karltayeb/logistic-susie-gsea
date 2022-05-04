c0a7c5e6933a5008b62f28d530b06cb685596f70
karltayeb <karl.tayeb7@gmail.com>
2022-05-04 10:42:48 -0500

    add glmnet

--- code/target_score_simulation.R
+++ code/target_score_simulation.R
@@ -5,15 +5,15 @@ score_thresh_single <- function(prediction, label, thresh=0.9, flip = F) {
     prediction = 1 - prediction
   }
   unlist(list(
-    TP = sum((prediction > thresh) & (label != 0)),
-    FP = sum((prediction > thresh) & (label == 0)),
-    TN = sum((prediction <= thresh) & (label == 0)),
-    FN = sum((prediction <= thresh) & (label != 0)),
+    TP = sum((prediction >= thresh) & (label != 0)),
+    FP = sum((prediction >= thresh) & (label == 0)),
+    TN = sum((prediction < thresh) & (label == 0)),
+    FN = sum((prediction < thresh) & (label != 0)),
     thresh=thresh
   ))
 }
 
-THRESH = seq(0, 1, 0.01)
+THRESH = c(seq(0, 0.98, 0.01), 1 - 10^(-(2:20)))
 
 score_phyper = function(fit, sim){
   map_dfr(THRESH, ~score_thresh_single(fit$pHypergeometric, sim$active, thresh=.x, flip=T))
@@ -21,4 +21,9 @@ score_phyper = function(fit, sim){
 
 score_pip = function(fit, sim){
   map_dfr(THRESH, ~score_thresh_single(fit$pip, sim$active, thresh=.x, flip=F))
+}
+
+score_cvglmnet = function(fit, sim){
+  nonzero <- tail(as.integer(coef(fit, s = "lambda.min")[,1] != 0), -1)
+  map_dfr(THRESH, ~score_thresh_single(nonzero, sim$active, thresh=.x, flip=F))
 }
