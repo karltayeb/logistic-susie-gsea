45c8662b661dd13a34ba5c90e7d4c9d4311d6529
karltayeb <karl.tayeb7@gmail.com>
2022-07-13 16:55:29 -0500

    b

--- code/target_run_enrichment.R
+++ code/target_run_enrichment.R
@@ -9,11 +9,11 @@ fit_affinity_prop_ora = function(ora, X, y){
 
   minusLogP <- -log(ora[match(names(idsInSet), ora$geneSet),]$pHypergeometric)
   minusLogP[minusLogP == Inf] <- -log(.Machine$double.eps)
-  apRes <- affinityPropagation(idsInSet, minusLogP)
+  apRes <- WebGestaltR::affinityPropagation(idsInSet, minusLogP)
   return(apRes)
 }
 
-fit_weighted_set_cover_ora = function(ora, X, y){
+fit_weighted_set_cover_ora = function(ora, X, y, n=10){
   sig.ora  <- ora %>% filter(pHypergeometric < 1e-2)
   X.sig <- X[, sig.ora$geneSet]
   X.y <- X.sig[(y == 1),]
@@ -24,7 +24,7 @@ fit_weighted_set_cover_ora = function(ora, X, y){
 
   minusLogP <- -log(ora[match(names(idsInSet), ora$geneSet),]$pHypergeometric)
   minusLogP[minusLogP == Inf] <- -log(.Machine$double.eps)
-  wscRes <- weightedSetCover(idsInSet, 1 / minusLogP, 10, 1)
+  wscRes <- WebGestaltR::weightedSetCover(idsInSet, 1 / minusLogP, n, 1)
   return(wscRes)
 }
 
@@ -35,6 +35,41 @@ run_enrichment = function(db, experiment, ptop, genesets, data){
                  '\n\tptop = ', ptop))
 
   bin.data <- gseasusie::prep_binary_data(genesets[[db]], data[[experiment]], ptop=ptop)
+  X <- bin.data$X
+  y <- bin.data$y
+
+  logistic.fit <- gseasusie::fit_logistic_susie_veb_boost(X, y, L=20)
+  ora <- gseasusie::fit_ora(X, y)
+
+  # other methods for summarizing ora results
+  # apres <- fit_affinity_prop_ora(ora, X, y)
+  # wscres <- fit_weighted_set_cover_ora(ora, X, y)
+
+  message('fitting linear susie')
+  tictoc::tic()
+  linear_fit <- susieR::susie(X, y, L=20)
+  tictoc::toc()
+
+  marginal_regression <- gseasusie::fit_marginal_regression_jax(X, y)
+  residual_regression <- gseasusie::fit_residual_regression_jax(X, y, logistic.fit)
+
+  res <- tibble(
+    y = list(y),
+    fit=list(logistic.fit),
+    ora=list(ora),
+    marginal_reg = list(marginal_regression),
+    residual_reg = list(residual_regression)
+  )
+  return(res)
+}
+
+run_enrichment2 = function(db, experiment, thresh, genesets, data){
+  message(paste0('Fitting logistic susie...',
+                 '\n\tExperiment = ', experiment,
+                 '\n\tDatabase = ', db,
+                 '\n\tthresh = ', thresh))
+
+  bin.data <- gseasusie::prep_binary_data(genesets[[db]], data[[experiment]], thresh=thresh)
   X <- bin.data$X
   y <- bin.data$y
 
@@ -133,6 +168,68 @@ enrichment_target_factory <- function(prefix, data_loader, .genesets, .propgenes
         enrichment_summary = list(summarise_enrichment(
           fit, ora, marginal_reg, residual_reg, genesets[[db]]$geneSetDes))) %>%
       select(db, experiment, ptop, enrichment_summary)
+  )))
+  summary_target_pattern = substitute(map(fit_target_sym))
+  summary_target <- tar_target_raw(
+    name = summary_target_name,
+    command = summary_target_command,
+    pattern = summary_target_pattern
+  )
+  return(list(data_target, meta_target, fit_target, summary_target))
+}
+
+# use .tresh instead of .propgenes
+enrichment_target_factory2 <- function(prefix, data_loader, .genesets, .thresh, .experiments=NULL){
+  # load data target
+  data_target_name <- paste0(prefix, '_data')
+  data_target_sym <- rlang::sym(data_target_name)
+  data_target <- tar_target_raw(data_target_name, substitute(data_loader()))
+
+  # meta target
+  meta_target_name <- paste0(prefix, '_meta')
+  meta_target_sym <- rlang::sym(meta_target_name)
+  by <- c('db', 'thresh')
+  tidy_eval <- targets::tar_option_get("tidy_eval")
+  command <- substitute(eval(rlang::expr(crossing(
+    db=.genesets,
+    experiment= if(is.null(.experiments)) names(data_target_sym) else intersect(names(data_target_sym), .experiments) ,
+    thresh = .thresh)
+  )))
+  meta_target_command <- tarchetypes:::tar_group_by_command(command, by, tidy_eval)
+  meta_target <- tar_target_raw(
+    name = meta_target_name,
+    command = meta_target_command,
+    iteration='group'
+  )
+
+  # fit target
+  fit_target_name <- paste0(prefix, '_enrichment_fit')
+  fit_target_sym <- rlang::sym(fit_target_name)
+  fit_target_command <- substitute(eval(rlang::expr(
+    meta_target_sym %>%
+      rowwise() %>%
+      mutate(
+        enrichment = list(run_enrichment2(db, experiment, thresh, genesets, data_target_sym))
+      ) %>%
+      ungroup() %>%
+      unnest(enrichment)
+  )))
+  fit_target_pattern = substitute(map(meta_target_sym))
+  fit_target <- tar_target_raw(
+    name = fit_target_name,
+    command = fit_target_command,
+    pattern = fit_target_pattern
+  )
+
+  # summary target
+  summary_target_name <- paste0(prefix, '_enrichment_summary')
+  summary_target_command <- substitute(eval(rlang::expr(
+    fit_target_sym %>%
+      rowwise() %>%
+      mutate(
+        enrichment_summary = list(summarise_enrichment(
+          fit, ora, marginal_reg, residual_reg, genesets[[db]]$geneSetDes))) %>%
+      select(db, experiment, thresh, enrichment_summary)
   )))
   summary_target_pattern = substitute(map(fit_target_sym))
   summary_target <- tar_target_raw(
